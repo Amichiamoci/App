d0ebded677f28c67d5f0e89cebaa94f0a3beafcd
Richie314 <richieinleghorn@gmail.com>
2024-08-21 12:14:06 +0200

    Many docker improvements

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@
 
 # Base image (contains server and http)
 FROM php:8.3-apache AS base
-
+ENV BASE_PATH=/
+ENV APP_PATH=/
 # Install php extensions
 ADD --chmod=0755 https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions /usr/local/bin/
 RUN install-php-extensions zip pgsql mysqli pdo pdo_mysql pdo_pgsql pdo_odbc sqlite3 ldap zip curl ffi fileinfo ftp gettext imap mbstring intl sockets
@@ -22,11 +23,15 @@ RUN composer update --no-interaction --no-progress
 FROM base AS final
 WORKDIR /var/www/html/
 COPY --from=composer /var/www/html/vendor/ ./vendor/
+COPY --from=composer /var/www/html/var/ ./var/
+
+COPY ./template.htaccess ./.htaccess
 
 # Enable mod_rewrite.c
 RUN a2enmod rewrite
 
 RUN chmod +x bin/console
+RUN chmod -R 777 ./var/cache/
 
 # Download packages
 RUN php bin/console importmap:install
--- assets/theme-handling.js
+++ assets/theme-handling.js
@@ -1,51 +1,55 @@
-let default_theme = 'light';
+(() => {
 
-const meta_color_scheme = document.querySelector('meta[name="color-scheme"]');
-if (meta_color_scheme) {
-    default_theme = meta_color_scheme.getAttribute('content').split(' ')[0];
-}
 
-const local_theme = window.localStorage.getItem('theme');
-const html = document.documentElement;
-const possible_themes = ['light', 'dark'];
-let current_theme_index = 0;
+    let default_theme = 'light';
 
-function setTheme(theme) {
-    console.log('Setting \'' + theme + '\' theme');
-    possible_themes.forEach(t => {
-        if (t === theme) {
-            html.classList.add(t);
-        } else {
-            html.classList.remove(t);
+    const meta_color_scheme = document.querySelector('meta[name="color-scheme"]');
+    if (meta_color_scheme) {
+        default_theme = meta_color_scheme.getAttribute('content').split(' ')[0];
+    }
+    
+    const local_theme = window.localStorage.getItem('theme');
+    const html = document.documentElement;
+    const possible_themes = ['light', 'dark'];
+    let current_theme_index = 0;
+    
+    function setTheme(theme) {
+        console.log('Setting \'' + theme + '\' theme');
+        possible_themes.forEach(t => {
+            if (t === theme) {
+                html.classList.add(t);
+            } else {
+                html.classList.remove(t);
+            }
+        });
+        if (window.localStorage.getItem('theme') !== theme) {
+            window.localStorage.setItem('theme', theme);
+        }
+        current_theme_index = possible_themes.indexOf(theme);
+    }
+    
+    function rotateTheme() {
+        current_theme_index = (current_theme_index + 1) % possible_themes.length;
+        setTheme(possible_themes[current_theme_index]);
+    }
+    
+    const change_themes_links = [...document.querySelectorAll('a[data-change-theme]')];
+    change_themes_links.forEach(a => {
+        
+        a.onclick = evt => {
+            evt.preventDefault();
+            rotateTheme();
+        }
+        
+        a.oncontextmenu = evt => evt.preventDefault();
+        if (!a.href.startsWith('javascript:')) {
+            a.href= "javascript:rotateTheme()";
         }
     });
-    if (window.localStorage.getItem('theme') !== theme) {
-        window.localStorage.setItem('theme', theme);
-    }
-    current_theme_index = possible_themes.indexOf(theme);
-}
-
-function rotateTheme() {
-    current_theme_index = (current_theme_index + 1) % possible_themes.length;
-    setTheme(possible_themes[current_theme_index]);
-}
-
-const change_themes_links = [...document.querySelectorAll('a[data-change-theme]')];
-change_themes_links.forEach(a => {
     
-    a.onclick = evt => {
-        evt.preventDefault();
-        rotateTheme();
+    if (local_theme) {
+        setTheme(local_theme);
+    } else {
+        setTheme(default_theme);
     }
-    
-    a.oncontextmenu = evt => evt.preventDefault();
-    a.href= "javascript:rotateTheme()";
-});
-
-
-
-if (local_theme) {
-    setTheme(local_theme);
-} else {
-    setTheme(default_theme);
-}
+})();
