034b14467d4a039fffb5b58afd1002cba1c63b7f
Richie314 <richieinleghorn@gmail.com>
2025-04-08 22:08:51 +0200

    Better login handling

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,26 +2,47 @@
 
 namespace App\Controller;
 
+use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/app_login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(
+        AuthenticationUtils $authenticationUtils, 
+        CsrfTokenManagerInterface $csrfTokenManager,
+    ): Response
     {
+        if ($this->getUser() !== null)
+        {
+            return $this->redirectToRoute(route: 'home');
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        if ($error !== null)
+        {
+            $this->addFlash(type: 'error', message: $error->getMessage());
+        }
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        $csrfToken = $csrfTokenManager->getToken(tokenId: 'authenticate')->getValue();
+        $form = $this->createForm(
+            type: LoginFormType::class, 
+            options: [
+                'csrf_token' => $csrfToken,
+            ]
+        );
 
-        return $this->render(view: 'security/login.html.twig', parameters: [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
+        return $this->render(
+            view: 'security/login.html.twig', 
+            parameters: [ 
+                'loginForm' => $form,
+            ],
+        );
     }
 
     #[Route(path: '/app_logout', name: 'app_logout')]
---
+++ src/Form/LoginFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginFormType extends AbstractType
+{
+    public function __construct(
+        private readonly string $csrfTokenId = 'authenticate',
+    ) { }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(child: '_username', type: EmailType::class, options: [
+                'label' => 'Email',
+                'attr' => [
+                    'autocomplete' => 'email',
+                    'autofocus' => true,
+                ],
+                'required' => true,
+            ])
+            ->add(child: '_password', type: PasswordType::class, options: [
+                'label' => 'Password',
+                'required' => true,
+            ])
+            ->add(child: '_remember_me', type: CheckboxType::class, options: [
+                'label'    => 'Resta collegato',
+                'required' => false,
+                'mapped'   => false,
+            ])
+            ->add(child: '_csrf_token', type: HiddenType::class, options: [
+                'mapped' => false,
+                'data' => $options['csrf_token'],
+            ])
+            ->add(child: '_login', type: SubmitType::class, options: [
+                'label' => 'Accedi'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'method' => 'POST',
+        ]);
+        $resolver->setRequired('csrf_token');
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return '';
+    }
+}
