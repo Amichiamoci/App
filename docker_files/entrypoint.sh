f98ae53e415f1f6877ccd20027f717720abf5bde
Richie314 <richieinleghorn@gmail.com>
2025-05-18 00:23:22 +0200

    Missing / in path

--- docker_files/entrypoint.sh
+++ docker_files/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 UPLOAD_DIRECTORY=/app/var/uploads
 mkdir -p $UPLOAD_DIRECTORY
-echo 'deny from all' > $UPLOAD_DIRECTORY.htaccess
+echo 'deny from all' > $UPLOAD_DIRECTORY/.htaccess
 chown -R www-data:www-data $UPLOAD_DIRECTORY
 
 apache2ctl -D FOREGROUND
