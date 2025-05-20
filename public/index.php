0071bcd3ef575e734b89b2bca2cc7304db5184a7
Richie314 <richieinleghorn@gmail.com>
2025-05-20 10:07:45 +0200

    Changed proxy config

--- public/index.php
+++ public/index.php
@@ -4,6 +4,19 @@ use App\Kernel;
 
 require_once dirname(path: __DIR__) . '/vendor/autoload_runtime.php';
 
+/*
+if (!empty($_SERVER['HTTP_CF_CONNECTING_IP']))
+{
+    $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+}
+if (!empty($_SERVER['HTTP_CF_VISITOR']))
+{
+    try {
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] = json_decode(json: $_SERVER['HTTP_CF_VISITOR'])['scheme'];
+    } catch (\Throwable) { }
+}
+*/
+
 return function (array $context): Kernel {
     return new Kernel(
         environment: $context['APP_ENV'],
---
+++ src/EventListener/RequestListener.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class RequestListener
+{
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        
+        if ($request->headers->get(key: 'x-forwarded-proto') === 'https')
+        {
+            $request->server->set(key: 'HTTPS', value: 'on');
+        }
+    }
+}
